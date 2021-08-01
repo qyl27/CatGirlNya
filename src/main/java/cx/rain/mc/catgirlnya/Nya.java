f58d93a57f3b61ea21e8c951648275a1fd15848c
秋雨落 <i@rain.cx>
2021-08-01 12:29:34 +0800

    :sparkles: 一点点微小的工作
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/cx/rain/mc/catgirlnya/Nya.java
+++ src/main/java/cx/rain/mc/catgirlnya/Nya.java
@@ -1,6 +1,9 @@
 package cx.rain.mc.catgirlnya;
 
+import cx.rain.mc.catgirlnya.item.ModItems;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,6 +17,9 @@ public class Nya {
 
     public Nya() {
         INSTANCE = this;
+
+        var bus = FMLJavaModLoadingContext.get().getModEventBus();
+        new ModItems(bus);
     }
 
     public static Nya getInstance() {
--- src/main/java/cx/rain/mc/catgirlnya/item/ModItems.java
+++ src/main/java/cx/rain/mc/catgirlnya/item/ModItems.java
@@ -3,18 +3,19 @@ package cx.rain.mc.catgirlnya.item;
 import cx.rain.mc.catgirlnya.Nya;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
-    public static final DeferredRegister<Item> REGISTRY =
+    public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, Nya.MODID);
 
     public ModItems(IEventBus bus) {
-        REGISTRY.register(bus);
+        ITEMS.register(bus);
         Nya.getInstance().getLogger().info("Registered items.");
     }
 
     // Items below.
-    
+    public static final RegistryObject<Item> PROGYNOVA = ITEMS.register("progynova", () -> new )
 }
